c0f34170088d19d90f9f27dcd5b7fa7154522b66
surajseth90 <surajseth90@gmail.com>
2021-05-07 13:14:59 +0530

    Added code for use case 2 of Binary Tree and thier test case also added successfully

--- src/test/java/com/bridgelabz/hashmapandbinarytreetest/MyBinaryTreeTest.java
+++ src/test/java/com/bridgelabz/hashmapandbinarytreetest/MyBinaryTreeTest.java
@@ -20,12 +20,22 @@ public class MyBinaryTreeTest {
 	@Test
 	public void givenNNumbersWhenAddedToBinaryTreeShouldReturnSizeN() {
 		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<Integer>();
-		int n = 10;
-		for (int i = 0; i < n; i++) {
-			myBinaryTree.add(i+1);
-		}
+		
+		myBinaryTree.add(56);
+		myBinaryTree.add(30);
+		myBinaryTree.add(22);
+		myBinaryTree.add(11);
+		myBinaryTree.add(3);
+		myBinaryTree.add(16);
+		myBinaryTree.add(40);
+		myBinaryTree.add(70);
+		myBinaryTree.add(60);
+		myBinaryTree.add(65);
+		myBinaryTree.add(63);
+		myBinaryTree.add(67);
+		myBinaryTree.add(95);
 		int size =myBinaryTree.getSize();
-		Assert.assertEquals(n, size);
+		Assert.assertEquals(13, size);
 	}
 }
  
